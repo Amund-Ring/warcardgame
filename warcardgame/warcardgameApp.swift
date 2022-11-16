3e5ad4925f2ce69923b24faaae3cf5c8b01ee04c
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2022-11-16 14:47:45 +0100

    Initial Commit

---
+++ warcardgame/warcardgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warcardgameApp.swift
+//  warcardgame
+//
+//  Created by Amund Ring on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct warcardgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
